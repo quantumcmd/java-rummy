2d73528397d4f560e59469253936f94777aada0e
Samuel <oluwamuyiwasamuel08@gmail.com>
2026-05-18 20:27:51 +0200

    feat: add GameLogger for in-memory action logging

---
+++ src/main/java/logging/GameLogger.java
@@ -0,0 +1,30 @@
+package logging;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameLogger {
+    private List<String> logs;
+
+    public GameLogger(){
+        this.logs = new ArrayList<>();
+    }
+
+    // Instead of writing to a file every single time a player moves (which is slow),
+    // I'm storing the logs in a List in memory
+    public void logAction(String action){
+        logs.add(action);
+    }
+
+    public void exportLogsToFile(String fileName){
+        try(FileWriter writer = new FileWriter(fileName)){
+            for(String message : logs){
+                writer.write(message + "\n");
+            }
+        } catch (IOException e){
+            System.out.println("Error saving logs: " + e.getMessage());
+        }
+    }
+}
