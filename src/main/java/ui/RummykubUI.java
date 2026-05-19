2261c7dadf351b46a30d77aa543ed2268038a8c4
Samuel <oluwamuyiwasamuel08@gmail.com>
2026-05-19 10:07:22 +0200

    feat: add RummykubUI with meld selection prompt

---
+++ src/main/java/ui/RummykubUI.java
@@ -0,0 +1,64 @@
+package ui;
+
+import model.GameState;
+
+import java.util.Scanner;
+
+public class RummykubUI extends InGameUI{
+
+    public RummykubUI(Scanner scanner) {
+        super(scanner);
+    }
+
+    @Override
+    protected void printOptions(){
+        System.out.println("\n=== Rummykub Options ===");
+        System.out.println("1 -> Draw from Deck");
+        System.out.println("2 -> Play a new meld (from hand)");
+        System.out.println("3 -> Add tile to existing meld");
+        System.out.println("4 -> Discard a piece (End turn)");
+        System.out.println("S -> Save and quit");
+        System.out.println("==========================\n");
+    }
+
+    @Override
+    public String promptTurnAction(){
+        while (true){
+            printOptions();
+
+            System.out.print("Choose an action (1-4) || S: ");
+            String input = scanner.nextLine();
+
+            switch (input){
+                case "1", "2", "3", "4", "S", "s":
+                    return input;
+                default:
+                    System.out.println("Invalid choice. Enter a number between 1 and 4 || S");
+                    break;
+            }
+        }
+    }
+
+    public int promptMeldSelection(GameState state){
+        while(true){
+            for(int i = 0; i < state.getBoard().getMeldCount(); i++){
+                System.out.println(i + ": " + state.getBoard().getMeld(i).getPieces());
+            }
+
+            System.out.print("Enter a meld index: ");
+            String input = scanner.nextLine();
+
+            try{
+                int meldIndex = Integer.parseInt(input);
+                if(indexIsWithinRange(meldIndex, 0, state.getBoard().getMeldCount() - 1))
+                    return meldIndex;
+            } catch (NumberFormatException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
+    private boolean indexIsWithinRange(int index, int min, int max){
+        return index >= min && index <= max;
+    }
+}
