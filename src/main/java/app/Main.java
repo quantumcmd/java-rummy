96790e46cc31418e1743bba2eb2e715fb9f70a05
Samuel <oluwamuyiwasamuel08@gmail.com>
2026-05-19 17:05:31 +0200

    feat: Wire up the whole game

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,14 @@
 package app;
 
+import ui.MainMenu;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        MainMenu mainMenu = new MainMenu(scanner);
+        Game game = new Game(mainMenu);
+        game.start();
     }
 }
